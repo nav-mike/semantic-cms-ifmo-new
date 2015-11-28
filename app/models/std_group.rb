edf1372a2536fe0cf94910f8b1a25e7ece73e736
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-11-28 15:29:25 +0300

    add students group model (sql only)

---
+++ app/models/std_group.rb
@@ -0,0 +1,2 @@
+class StdGroup < ActiveRecord::Base
+end
---
+++ test/models/std_group_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StdGroupTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
