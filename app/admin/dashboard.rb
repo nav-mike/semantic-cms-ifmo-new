58e29bb4654f7d3b25a222273730cab8be4c5ede
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-11-29 21:58:56 +0000

    add pages to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,19 @@
 ActiveAdmin.register_page "Dashboard" do
+  pages = Page.all
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    panel 'Pages' do
+      table_for pages do
+        column 'name', :name
+        column 'title', :title
+        column 'path', :path
+        column '', :updated_at
+        column do |page|
+          link_to 'Preview', "#{page.path}", class: 'view_link member_link'
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
