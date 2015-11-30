d1f0e7e004b7a441097a4fe7c0f196286e66dbd1
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-11-30 17:32:05 +0000

    add new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'm.navrotskiy@gmail.com', password: '12345678', password_confirmation: '12345678')
+
+Department.create!(
+  name: 'Кафедра информатики и прикладной математики',
+  research_tags: [],
+  about: '',
+  university_name: 'Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики',
+  url: 'http://ipm.ifmo.ru',
+  css: '',
+  js: '',
+  layout: '',
+  logo: '',
+  index_page_path: '/pages/index'
+)
