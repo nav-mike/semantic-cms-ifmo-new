805cacbd0a527e2b0b86e3cbd90d0e9f30742903
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-12-08 21:17:58 +0300

    refactoring variable admin form

--- app/admin/variable.rb
+++ app/admin/variable.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Variable do
   permit_params :name, :title, :sparql, :can_removed
+
+  form do |f|
+    inputs '' do
+      f.input :name, label: 'Name of variable (use @<name> for access in page)'
+      f.input :title, label: 'Title (or comment)'
+      f.input :sparql, label: 'Sparql request'
+      actions
+    end
+  end
 end
